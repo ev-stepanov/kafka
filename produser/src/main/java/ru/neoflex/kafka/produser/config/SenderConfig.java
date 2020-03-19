119f051023bba94b9ab4ec5c868386f6be6db6fb
estepanov <estepanov@neoflex.ru>
2020-03-19 17:34:08 +0400

    refactoring pom.xml

--- produser/src/main/java/ru/neoflex/kafka/produser/config/SenderConfig.java
+++ produser/src/main/java/ru/neoflex/kafka/produser/config/SenderConfig.java
@@ -1,6 +1,5 @@
 package ru.neoflex.kafka.produser.config;
 
-
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
