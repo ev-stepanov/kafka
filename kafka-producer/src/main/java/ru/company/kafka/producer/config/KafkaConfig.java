45b207d0dc1c7afa850b3a039f1ecdf342ae5c04
Степанов Евгений <estepanov@neoflex.ru>
2020-06-18 17:11:37 +0400

    refactoring

--- kafka-producer/src/main/java/ru/company/kafka/producer/config/KafkaConfig.java
+++ kafka-producer/src/main/java/ru/company/kafka/producer/config/KafkaConfig.java
@@ -28,7 +28,6 @@ public class KafkaConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
         return props;
     }
 
